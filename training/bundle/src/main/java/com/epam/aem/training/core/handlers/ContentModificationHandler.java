8845a467842969fb7b2a5a233ecfba67c5f1057c
Hanna Yakubenka <Hanna_Yakubenka@epam.com>
2017-01-26 14:14:53 +0300

    Add simple EventHandler

---
+++ training/bundle/src/main/java/com/epam/aem/training/core/handlers/ContentModificationHandler.java
@@ -0,0 +1,38 @@
+package com.epam.aem.training.core.handlers;
+
+import java.util.Iterator;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Service;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventConstants;
+import org.osgi.service.event.EventHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.day.cq.wcm.api.PageEvent;
+import com.day.cq.wcm.api.PageModification;
+
+@Component
+@Service(EventHandler.class)
+@Properties({
+	@Property(name=EventConstants.EVENT_TOPIC, value=PageEvent.EVENT_TOPIC)
+})
+public class ContentModificationHandler implements EventHandler {
+	
+	private final Logger LOG = LoggerFactory.getLogger(this.getClass().getName());
+
+	@Override
+	public void handleEvent(Event event) {
+		PageEvent pgEvent = PageEvent.fromEvent(event);
+		Iterator<PageModification> modifications = pgEvent.getModifications();
+		
+		while(modifications.hasNext()){
+			PageModification modification = modifications.next();
+			LOG.debug(modification.toString());
+		}
+	}
+
+}
