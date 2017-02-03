cf7109d82e7ce229dbace4f8b06d2678b40bc2cc
Hanna Yakubenka <Hanna_Yakubenka@epam.com>
2017-02-03 18:52:03 +0300

    Added logic to execute() method in MovePageWorkflow. Added OR split to workflow model.

--- training/workflowBundle/src/main/java/com/epam/aem/training/workflow/processes/MovePageWorkflow.java
+++ training/workflowBundle/src/main/java/com/epam/aem/training/workflow/processes/MovePageWorkflow.java
@@ -1,16 +1,24 @@
 package com.epam.aem.training.workflow.processes;
 
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Service;
+import org.apache.jackrabbit.JcrConstants;
+import org.apache.jackrabbit.core.fs.FileSystem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.day.cq.workflow.WorkflowException;
-import com.day.cq.workflow.WorkflowSession;
-import com.day.cq.workflow.exec.WorkItem;
-import com.day.cq.workflow.exec.WorkflowProcess;
-import com.day.cq.workflow.metadata.MetaDataMap;
+import com.adobe.granite.workflow.PayloadMap;
+import com.adobe.granite.workflow.WorkflowException;
+import com.adobe.granite.workflow.WorkflowSession;
+import com.adobe.granite.workflow.exec.WorkItem;
+import com.adobe.granite.workflow.exec.WorkflowData;
+import com.adobe.granite.workflow.exec.WorkflowProcess;
+import com.adobe.granite.workflow.metadata.MetaDataMap;
 
 @Component
 @Service(WorkflowProcess.class)
@@ -19,11 +27,33 @@ public class MovePageWorkflow implements WorkflowProcess{
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
 	
+	private static final String PATH_TO_MOVE = "pathToMove";
+	
 	@Override
-	public void execute(WorkItem workItem, WorkflowSession wfSession, MetaDataMap metaDataMap)
+	public void execute(WorkItem workItem, WorkflowSession workflowSession, MetaDataMap metaDataMap)
 			throws WorkflowException {
 		logger.debug("Training process executed: called from Java code");
-		logger.debug("Payload: " + workItem.getWorkflowData().getPayload());
+		WorkflowData workflowData = workItem.getWorkflowData();
+		if(workflowData.getPayloadType().equals(PayloadMap.TYPE_JCR_PATH)){
+			String currentPath = workflowData.getPayload().toString();
+			Session jcrSession = workflowSession.adaptTo(Session.class);
+			try {
+				Node pageNode = jcrSession.getNode(currentPath);
+				if(pageNode != null){
+					Node contentNode = pageNode.getNode(JcrConstants.JCR_CONTENT);
+					if(contentNode.hasProperty(PATH_TO_MOVE)){
+						String pathToMove = contentNode.getProperty(PATH_TO_MOVE).getString();
+						if(jcrSession.nodeExists(pathToMove) && !pathToMove.isEmpty() && !pathToMove.equals(currentPath)){
+							jcrSession.move(currentPath, pathToMove + FileSystem.SEPARATOR + pageNode.getName());
+							jcrSession.save();
+						}
+					}
+				}
+			} catch (RepositoryException e) {
+				logger.error(e.getMessage());
+			}
+			
+		}
 	}
 
 }
