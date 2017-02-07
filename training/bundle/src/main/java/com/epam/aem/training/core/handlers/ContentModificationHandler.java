f53b231735bdb85edd796e0676b1f807e9be4f53
Hanna Yakubenka <Hanna_Yakubenka@epam.com>
2017-02-07 20:41:19 +0300

    Using wcm.api. Added private constructor and closeResourceResolver method to ResourceResolverUtil.

--- training/bundle/src/main/java/com/epam/aem/training/core/handlers/ContentModificationHandler.java
+++ training/bundle/src/main/java/com/epam/aem/training/core/handlers/ContentModificationHandler.java
@@ -2,18 +2,13 @@ package com.epam.aem.training.core.handlers;
 
 import java.util.Iterator;
 
-import javax.jcr.Node;
-import javax.jcr.RepositoryException;
-import javax.jcr.Session;
-import javax.jcr.version.Version;
-import javax.jcr.version.VersionManager;
-
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Properties;
 import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.api.resource.LoginException;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.osgi.service.event.Event;
 import org.osgi.service.event.EventConstants;
@@ -21,9 +16,11 @@ import org.osgi.service.event.EventHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.day.cq.commons.jcr.JcrConstants;
+import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.api.PageEvent;
+import com.day.cq.wcm.api.PageManager;
 import com.day.cq.wcm.api.PageModification;
+import com.day.cq.wcm.api.WCMException;
 import com.epam.aem.training.core.utils.ResourceResolverUtil;
 
 @Component
@@ -51,25 +48,21 @@ public class ContentModificationHandler implements EventHandler {
 			
 			if(modification.getPath().matches(REGEXP) && 
 					modification.getType().equals(PageModification.ModificationType.MODIFIED)){
+				ResourceResolver resourceResolver = null;
 				try {
-					Session session = ResourceResolverUtil.getServerResourceResolver(resolverFactory, 
-							EVENT_SERVICE).adaptTo(Session.class);
-					Node pageNode = session.getNode(modification.getPath());
-					Node contentNode = pageNode.getNode(JcrConstants.JCR_CONTENT);
-					if(contentNode.hasProperty(JcrConstants.JCR_DESCRIPTION)){
-						createVersion(pageNode, session);
+					 resourceResolver = ResourceResolverUtil.
+							getServerResourceResolver(resolverFactory, EVENT_SERVICE);
+					PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
+					Page page = pageManager.getContainingPage(modification.getPath());
+					if(page.getDescription() != null){
+						pageManager.createRevision(page);
 					}
-				} catch (LoginException | RepositoryException e) {
-					logger.error(e.getMessage());
+				} catch (LoginException | WCMException e) {
+					logger.error(e.getStackTrace().toString());
+				} finally{
+					ResourceResolverUtil.closeResourceResolver(resourceResolver);
 				}
 			}
 		}
 	}
-	
-	private void createVersion(Node page, Session session) throws RepositoryException{
-		VersionManager versionManager = session.getWorkspace().getVersionManager();
-		Version version = versionManager.checkpoint(page.getPath());
-		logger.debug(version.toString());
-	}
-
 }
--- training/bundle/src/main/java/com/epam/aem/training/core/utils/ResourceResolverUtil.java
+++ training/bundle/src/main/java/com/epam/aem/training/core/utils/ResourceResolverUtil.java
@@ -9,11 +9,21 @@ import org.apache.sling.api.resource.ResourceResolverFactory;
 
 public class ResourceResolverUtil {
 	
+	private ResourceResolverUtil(){
+		super();
+	}
+	
 	public static ResourceResolver getServerResourceResolver(ResourceResolverFactory 
 			resolverFactory, String subservice) throws LoginException{
 		Map<String, Object> authenticationInfo = new HashMap<>();
 		authenticationInfo.put(ResourceResolverFactory.SUBSERVICE, subservice);
 		return resolverFactory.getServiceResourceResolver(authenticationInfo);
+	}
+	
+	public static void closeResourceResolver(ResourceResolver resourceResolver){
+		if(resourceResolver != null && resourceResolver.isLive()){
+			resourceResolver.close();
+		}
 	}
 
 }
